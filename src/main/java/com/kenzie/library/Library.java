96866b5f6bc4d36a8bbe371cb4d9962a7aab974b
Tanner-Wills <tannerwills1@gmail.com>
2021-06-28 20:59:56 -0400

    editing addbook method to library

--- src/main/java/com/kenzie/library/Library.java
+++ src/main/java/com/kenzie/library/Library.java
@@ -1,36 +1,61 @@
 package com.kenzie.library;
-import java.util.*;
+import java.util.ArrayList;
+
 
 public class Library {
 
-    private List<Object> libraryContents;
-    private List<String> booksCheckedOut;
+    private ArrayList<Object> libraryContents;
+    private ArrayList<String> booksCheckedOut;
     private Object Book;
 
-    public Library(){
 
+    //Constructor
+    public Library(){
         this.libraryContents = new ArrayList<Object>();
         this.booksCheckedOut = new ArrayList<String>();
-
     }
 
-    public Object addBook(Book book){
-        return libraryContents.add(Book);
+    //Add Library methods
+    public void addBook(Book book){
+        libraryContents.add(book);
     }
 /*
     public String[] listBooks(){
         String[] bookList = new String[libraryContents.size()];
         if (libraryContents.size() > 0){
-            for (int book = 0; book < libraryContents.size(); book++){
+            for (int book = 0; book <= libraryContents.size(); book++){
                 Book currentBook = (Book) libraryContents.get(book);
                 String bookTitle = currentBook.getTitle();
                 bookList[book] = bookTitle;
-                return bookList;
+
             }
-        } else {
+            } else {
             System.out.println("There are no books in the library!");
         }
-        return null;
+        return System.out.println(bookList);;
     }
 */
-}
+
+    public Book checkoutBook(String bookTitle){
+        //Loop through libraryContents to see if bookTitle is in there
+        for (int i = 0; i < libraryContents.size(); i++){
+            Book currentBook = (Book) libraryContents.get(i);
+            String currentTitle = currentBook.getTitle();
+
+            if (bookTitle == currentTitle && !booksCheckedOut.contains(bookTitle)) {
+                booksCheckedOut.add(bookTitle);
+                System.out.println("book checked out");
+                return currentBook;
+            }
+
+            else if (bookTitle == currentTitle && booksCheckedOut.contains(bookTitle)){
+                System.out.println("test");
+                throw new BookNotAvailableException(bookTitle);
+                }
+            }
+        throw new BookNotFoundException(bookTitle);
+        }
+
+    }
+
+
--- src/main/java/com/kenzie/library/TestScript.java
+++ src/main/java/com/kenzie/library/TestScript.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class TestScript {
 
     public static void main(String[] args) {
-        String [] myContents = { "I", "Love", "Reading" };
+        String[] myContents = {"I", "Love", "Reading"};
+
+        Book myBook = new Book("Sorcerer's Stone", "J.K. Rowling", Genre.SCIENCE_FICTION, myContents);
+
+
         Library library = new Library();
-        Book myBook = new Book("Sorcerer's Stone","J.K. Rowling",Genre.SCIENCE_FICTION, myContents);
-        System.out.println("Title: " + myBook.getTitle());
-        System.out.println("Author: " + myBook.getAuthor());
-        System.out.println("Genre: " + myBook.getGenre());
-        System.out.println("Number of pages: " + myBook.getNumPages());
-        System.out.println("Current Page: " + myBook.getCurrentPageNumber());
-        System.out.println(library.addBook(myBook));
-        System.out.println(Arrays.toString(library.listBooks()));
+        library.addBook(myBook);
+        library.checkoutBook("Sorcerer's Stone");
+        library.checkoutBook("Scorcerer's Stone");
 
 
+
+        //library.listBooks();
+
+        }
     }
 
-}
